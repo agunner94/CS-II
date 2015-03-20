cdebcda6bb4e6a94b213275c8146878cdb0e98ef
agunner94 <agunner94@gmail.com>
2015-03-19 20:10:31 -0500

    Create Queue Linked.h

---
+++ Queue Linked.h
@@ -0,0 +1,44 @@
+template <class ItemType>
+struct NodeType;
+
+template <class ItemType>
+class QueType
+{
+public:
+    QueType();
+    // Class constructor.
+    // Because there is a default constructor, the precondition that the
+    // queue has been initialized is omitted.
+
+    ~QueType();
+    // Class destructor.
+
+    void MakeEmpty();
+    // Function: Initializes the queue to an empty state.
+    // Post: Queue is empty.
+
+    bool IsEmpty() const;
+    // Function: Determines whether the queue is empty.
+    // Post: Function value = (queue is empty)
+
+    bool IsFull() const;
+    // Function: Determines whether the queue is full.
+    // Post: Function value = (queue is full)
+
+    void Enqueue(ItemType newItem);
+    // Function: Adds newItem to the rear of the queue.
+    // Pre:  Queue is not full.
+    // Post: newItem is at the rear of the queue.
+
+    void Dequeue(ItemType& item);
+    // Function: Removes front item from the queue and returns it in item.
+    // Pre:  Queue is not empty.
+    // Post: Front element has been removed from the queue.
+    //       item is a copy of the removed element.
+
+private:
+    NodeType<ItemType>* qFront;
+    NodeType<ItemType>* qRear;
+};
+
+#include "Queue linked.cpp"
